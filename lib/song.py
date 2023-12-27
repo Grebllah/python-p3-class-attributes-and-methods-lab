a98280669b4406be9a0db75de844793b1fd50d4a
James Hallberg <Grebllah81788@gmail.com>
2023-12-27 12:07:07 -0500

    class attritbutes and methods lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,25 @@
 class Song:
-    pass
+    
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_song_to_count(name, artist, genre)
+
+    @classmethod
+    def add_song_to_count(cls, name, artist, genre, increment=1,):
+        cls.count += increment
+        if not genre in Song.genres:
+            Song.genres.append(genre)
+            Song.genre_count[genre] = 1
+        else: Song.genre_count[genre] += 1
+        if not artist in Song.artists:
+            Song.artists.append(artist)
+            Song.artist_count[artist] = 1
+        else: Song.artist_count[artist] += 1
